97453acfeee0a9050e19d8f54fce528eb9722d47
jennifermccarthy <jennifernward@gmail.com>
2014-08-29 23:59:05 +0000

    allow edit and update of a current recipe object

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,9 +16,23 @@ class RecipesController < ApplicationController
       flash[:notice] = "This recipe has been saved to your catalog"
       redirect_to("/recipes")
     else
-      render("recipes/new/html.erb")
+      render("recipes/new.html.erb")
     end
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+    render('recipes/edit.html.erb')
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(params[:recipe])
+      flash[:notice] = "This recipe has been successfully updated"
+      redirect_to("/recipes")
+    else
+      render("recipes/edit.html.erb")
+    end
+  end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   match('recipes', { :via => :get, :to => 'recipes#index' })
   match('recipes/new', { :via => :get, :to => 'recipes#new' })
   match('recipes', { :via => :post, :to => 'recipes#create' })
+  match('recipes/:id/edit', { :via => :get, :to => 'recipes#edit'})
+  match('recipes/:id', { :via => [:patch, :put], :to => 'recipes#update'})
    
  
 end
